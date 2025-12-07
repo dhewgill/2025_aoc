24bd402254c1e965049636208019f9906fb3d66a
Dale Hewgill <dhewgill@gmail.com>
2025-12-07 10:46:09 -0800

    Day02 part 2 in progress.

--- day02.py
+++ day02.py
@@ -9,8 +9,8 @@ from common import parse_file
 
 # ####################################
 # --------------  Main  --------------
-P1_DATFILE = r"data/d2p1_ex.txt" #r"data/d2p1.txt" #r"data/d2p1_ex.txt"
-P2_DATFILE = P1_DATFILE #r"data/d2p1_ex.txt"
+P1_DATFILE = r"data/d2p1.txt" #r"data/d2p1_ex.txt"
+P2_DATFILE = r"data/d2p1_ex.txt"
 
 def main(argv=None):
     if argv is None:
@@ -35,11 +35,11 @@ def main(argv=None):
 
     # Day 2, part 1.
     d2p1 = do_d2p1(P1_DATFILE)
-    logging.info(f"Part 1: Secret code is {d2p1}") # 
+    logging.info(f"Part 1: Secret code is {d2p1}") # 12586854255
 
     # Day 2, part 2.
-    # d2p2 = do_d2p2(P2_DATFILE)
-    # logging.info(f"Part 2: Secret code is {d2p2}") # 
+    d2p2 = do_d2p2(P2_DATFILE)
+    logging.info(f"Part 2: Secret code is {d2p2}") # 
 
     print("\n\nEnd")
 
@@ -56,19 +56,75 @@ def parse_input(raw_data: list[str]) -> list[tuple[int, int]]:
     return parsed_data
 
 
+def chunker(seq: list, size: int) -> list[list]:
+    return [seq[pos:pos + size] for pos in range(0, len(seq), size)]
+
+
+def find_invalid_ids(
+    id_range: tuple[int, int], check_subs:bool = False
+) -> list[int]:
+    """
+    Invalid IDs are those that are that contain a duplicated sequence of digits
+    such as: 11, 1212, 123123, etc.
+    """
+    invalid_ids = []
+    r_lo, r_hi = id_range
+
+    for id_num in range(r_lo, r_hi + 1):
+        id_str = str(id_num)
+        id_len = len(id_str)
+
+        if id_len == 1:
+            continue
+        if id_len % 2 != 0:
+            if check_subs:
+                if all(i == id_str[0] for i in id_str):
+                    invalid_ids.append(id_num)
+            continue
+
+        half_len = id_len // 2
+        first_half = id_str[0:half_len]
+        second_half = id_str[half_len:id_len]
+        if first_half == second_half:
+            invalid_ids.append(id_num)
+            continue
+
+        if check_subs:
+            for n in range(2, half_len):
+                if id_len % n != 0:
+                    continue
+                subs = chunker(id_str, n)
+                if all(s == subs[0] for s in subs):
+                    invalid_ids.append(id_num)
+                    break
+
+    return invalid_ids
+
+
 def do_d2p1(datafile: str) -> int:
     raw_data = parse_file(datafile)
     parsed_data = parse_input(raw_data)
-    logging.info("Parsed data: %s", parsed_data)
+    logging.debug("Parsed data: %s", parsed_data)
+    invalid_id_sum = 0
+    for id_range in parsed_data:
+        invalid_ids = find_invalid_ids(id_range)
+        logging.debug("Invalid IDs in range %s: %s", id_range, invalid_ids)
+        invalid_id_sum += sum(invalid_ids)
 
-    return None
+    return invalid_id_sum
 
 
 def do_d2p2(datafile: str) -> int:
     raw_data = parse_file(datafile)
     parsed_data = parse_input(raw_data)
+    logging.debug("Parsed data: %s", parsed_data)
+    invalid_id_sum = 0
+    for id_range in parsed_data:
+        invalid_ids = find_invalid_ids(id_range, True)
+        logging.info("Invalid IDs in range %s: %s", id_range, invalid_ids)
+        invalid_id_sum += sum(invalid_ids)
 
-    return None
+    return invalid_id_sum
 
 
 # ####################################
