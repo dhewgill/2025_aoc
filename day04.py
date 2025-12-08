6d1acfb62f98481df20ae2540dd80bfa7512507c
Dale Hewgill <dhewgill@gmail.com>
2025-12-07 16:56:41 -0800

    Day 4, part 1 solution.

--- day04.py
+++ day04.py
@@ -46,14 +46,54 @@ def main(argv=None):
 
 # ####################################
 # --------------  Util  --------------
+def print_grid(grid: list[str]) -> None:
+    logging.info("Grid:")
+    for row in grid:
+        print(row)
+
+
+def count_neigbbours(grid: list[str], row:int, col:int, occupied:str = "@") -> int:
+    n_rows = len(grid)
+    n_cols = len(grid[0])
+    deltas = [(-1, -1), (-1, 0), (-1, 1),
+              (0, -1),          (0, 1),
+              (1, -1),  (1, 0), (1, 1)]
+    n_occupied = 0
+    for dr, dc in deltas:
+        r = row + dr
+        c = col + dc
+        if 0 <= r < n_rows and 0 <= c < n_cols:
+            if grid[r][c] == occupied:
+                n_occupied += 1
+
+    return n_occupied
+
+
+def parse_input(raw_data: list[str]) -> list[str]:
+    parsed_data = []
+    for line in raw_data:
+        parsed_data.append(line.strip())
+    return parsed_data
 
 
 def do_d4p1(datafile: str) -> int:
     raw_data = parse_file(datafile)
     logging.debug("Raw data: %s", raw_data)
 
+    grid = parse_input(raw_data)
+    #print_grid(grid)
 
-    return None
+    num_moveable = 0
+    n_cols = len(grid[0]) # Assume all rows same length.
+    for row_num, row in enumerate(grid):
+        for col in range(n_cols):
+            if row[col] != "@":
+                continue
+            n_occupied = count_neigbbours(grid, row_num, col)
+            logging.debug("Cell (%d, %d) has %d occupied neighbours", row_num, col, n_occupied)
+            if n_occupied < 4:
+                num_moveable += 1
+    return num_moveable
 
 
 def do_d4p2(datafile: str) -> int:
